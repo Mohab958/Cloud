11f337cf3040f7ad1344d50a7761d0eb6c3d5669
Mohab958 <163203630+Mohab958@users.noreply.github.com>
2024-04-22 02:48:38 +0200

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -17,7 +17,7 @@ tokenize_words_without_stop_words = []
 for word in tokenize_words:
     if word not in stop_words:
         tokenize_words_without_stop_words.append(word)
-print("Stop words which got removed from the paragraph" , set(tokenize_words)-set(tokenize_words_without_stop_words))
+print("Stop words which got removed from the paragraph:" , set(tokenize_words)-set(tokenize_words_without_stop_words))
 
 
 words = Counter(tokenize_words_without_stop_words)
